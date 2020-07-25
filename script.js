8a2030f12e82d9f91f409d7261f26f48ba614b98
Anthony Irwin <anthonyirwin@gmail.com>
2020-07-25 13:59:03 -0400

    First success with weather API

--- script.js
+++ script.js
@@ -1,35 +1,14 @@
+let fromapi = fetch('http://api.openweathermap.org/data/2.5/weather?q=Louisville,us&APPID=6368a49d701b9248d5d30b549448ea4a')
+.then(function(res){console.log(res.json())})
 
-// Get the modal
-var modal = document.getElementById("myModal");
+document.getElementById('displaydata')
+.addEventListener('click', getText);
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+function getText(){
+  console.log(fromapi);
+    document.getElementById("headerone")
+    .innerHTML = "<h1>" + fromapi + "</h1>"
 }
 
 
-function inputBox() {
-document.getElementById("addField").innerHTML = Haha
-}
 
-function removeBox() {
-  document.getElementById("")
-}

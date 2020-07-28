6c16670fda38babf1f91611393c47fb1607a1545
Anthony Irwin <anthonyirwin@gmail.com>
2020-07-28 01:54:26 -0400

    graph function included

--- script.js
+++ script.js
@@ -1,42 +1,22 @@
 
-//event handler that runs getWeatherData function when button is clicked.
-document.getElementById('displaydata')
-.addEventListener('click', getWeatherData);
 
 
-//Function that assigns inputed text from zipinput to zip variable.
-
- const zipregex = /^\d{5}$/; 
-
-
-// function zipregexval() {
-
-
-//     zip = document.getElementById('zipinput').value
-
-//     zipfunction()
-      
-//     }
-
-
-
-
-
-
-// }
-
-function zipfunction() {
-    
-    
+//implementation of regex on zip code input, only 5 characters. 
+function zipfunction() {  
     zip = document.getElementById("zipinput").value
-    
-    
-    
-    getWeatherData()
-
-   
+    var regex = /^\d{1,5}$/;
+    if (regex.test(zip)){
+        console.log("true")
+        
+        //calls getweatherdataunction
+        getWeatherData() 
+    }
+    else
+    {
+        console.log("false")
+        document.getElementById('weatherdiv') = "Please Input a Valid Zip Code"
+    }
 }
-
 //function that fetches openweather API with the zip variable inserted into the html address. The API returns area data. 
 function getWeatherData(){
      // fetch('http://api.openweathermap.org/data/2.5/weather?q=Louisville,us&APPID=f360ccaab20b5b1f7087127ed1a6d955')
@@ -47,25 +27,52 @@ function getWeatherData(){
         document.getElementById('weatherdiv').innerHTML = "The temperature in " + data.name + " right now is" + " " + data.main.temp + " " + "Kelvin";
       })}
 
+
+//function for radio buttons
+
 function radiotempcheck() {
     if (document.getElementById("faranheit").checked){
 contemp = document.getElementById('fname').value
 findegree = parseFloat(contemp)
 console.log(contemp)
-document.getElementById('dataoutput').innerHTML =  ((findegree-273.15)*1.8)+32
+document.getElementById('dataoutput').innerHTML =  Math.round(((findegree-273.15)*1.8)+32)
     }
      else if (document.getElementById('celcius').checked)
         {
-
-        contemp = document.getElementById('fname').value     
-        findegree = parseFloat(contemp)
-
-        document.getElementById('dataoutput').innerHTML = findegree - 273.15
+            contemp = document.getElementById('fname').value     
+            findegree = parseFloat(contemp)
+            document.getElementById('dataoutput').innerHTML = Math.round(findegree - 273.15)
         }
+    }
+    //arrays for x and y data
 
-            }
+    let xarr = []
+    let yarr = []
+    
+    //function for retreiving and grahing api data from second api with 5-day weather data, every three hours.
 
+    function getplotdata(){
+        fetch('http://api.openweathermap.org/data/2.5/forecast?q=louisville&appid=f360ccaab20b5b1f7087127ed1a6d955')
+        .then((res) => res.json())
+        .then((data)=> {
+            
+            
+            for (i = 0; i < data.list.length; i++) {
+                
+            yarr.push(data.list[i].main.temp)
+            //  console.log(yarr)
+            xarr.push(data.list[i].dt_txt)
+            TESTER = document.getElementById('tester');
+            Plotly.newPlot( TESTER, [{
+            x: xarr,
+            y: yarr }], {
+            margin: { t: 0 } } );
 
+            document.getElementById('graphbox').innerhtml = "what's this?"
+    }
+    
+});
 
+    }
 
 
